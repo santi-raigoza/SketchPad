6d20978bb1f46cbc7470115ac8ace1069811c8e2
santi-raigoza <santiago.raigoza00@gmail.com>
2025-07-01 21:35:11 -0400

    Added eraser feature
    Eraser now works on click and fixed incorrect click events

--- javascript.js
+++ javascript.js
@@ -2,15 +2,19 @@ const main = document.querySelector(".main");
 const sketchPad = document.querySelector(".sketch-pad");
 const inputColor = document.querySelector("#color-input");
 const borderCheckBox = document.querySelector("#check-box");
+const eraser = document.querySelector(".eraser-icon");
 
 const defaultGridSize = 10;
-let currentColor = "#bccff3";
+let currentColor = "#bccff3"; //default color
+let erase = false;
 
 document.addEventListener("DOMContentLoaded", createGrid);
 
 sketchPad.addEventListener("click", colorBox);
 inputColor.addEventListener("input", colorChange);
 borderCheckBox.addEventListener("change", borderChange)
+eraser.addEventListener("click", eraseBox);
+
 
 function createGrid() {
     for (let i = 0; i < defaultGridSize; i++) {
@@ -27,7 +31,12 @@ function createGrid() {
 
 function colorBox(e) {
     currentGridBox = e.target;
-    currentGridBox.style.backgroundColor = currentColor;
+    
+    if (erase) {
+        currentGridBox.style.backgroundColor = "#ffffff";
+    } else {
+        currentGridBox.style.backgroundColor = currentColor;
+    }
 }
 
 function colorChange() {
@@ -45,4 +54,14 @@ function borderChange() {
             box.classList.remove("border-grid-box");
         }
     });
+}
+
+function eraseBox() {
+    if (erase) {
+        erase = false;
+        eraser.style.opacity = "1";
+    } else {
+        erase = true;
+        eraser.style.opacity = "0.7";
+    }
 }
